57d03c82f31cc26df81eedb057c27ef0a51af27f
vuggamramakanth <vuggamramakant@gmail.com>
2023-06-25 14:40:49 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+from django.urls import reverse
+
+class School(models.Model):
+    name=models.CharField(max_length=100) 
+    principal=models.CharField(max_length=100)  
+    location=models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail',kwargs={'pk':self.pk})
+    
+
+class Student(models.Model):
+    school=models.ForeignKey(School,on_delete=models.CASCADE,related_name='students')
+    sname=models.CharField(max_length=100)
+    sage=models.IntegerField()
+    def __str__(self) -> str:
+        return self.sname
+
+
+
+
+
+
+
+
+
---
+++ app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from app.models import *
+# Create your views here.
+from django.views.generic import TemplateView,ListView,DetailView
+
+class Home(TemplateView):
+    template_name='app/home.html'
+
+class SchoolList(ListView):
+    model = School
+    context_object_name='schools'
+
+class SchoolDetail(DetailView):
+    model=School
+    context_object_name='sclobject'
+
+
+
